aef66fae0adc995e07ef78374224c6cd71e4fc8f
신창화 <sch9270@gmail.com>
2023-09-20 17:08:20 +0900

    Drill-02 (2020182016 신창화)

--- Drill02.py
+++ Drill02.py
@@ -17,31 +17,36 @@ while(1):
 		character.draw_now(400 + 210 * math.sin(math.radians(i)), 300 + 210 * math.cos(math.radians(i)))
 		delay(0.01)
 		i = i + 1
-	while(x < 800):
+	while(x < 780):
 		clear_canvas_now()
 		grass.draw_now(400, 30)
 		character.draw_now(x, y)
 		x = x + 2
-	while(y < 510):
+		delay(0.01)
+	while(y < 560):
 		clear_canvas_now()
 		grass.draw_now(400, 30)
 		character.draw_now(x, y)
 		y = y + 2
-	while(x > 0):
+		delay(0.01)
+	while(x > 20):
 		clear_canvas_now()
 		grass.draw_now(400, 30)
 		character.draw_now(x, y)
 		x = x - 2
+		delay(0.01)
 	while(y > 90):
 		clear_canvas_now()
 		grass.draw_now(400, 30)
 		character.draw_now(x, y)
 		y = y - 2
+		delay(0.01)
 	while(x < 400):
 		clear_canvas_now()
 		grass.draw_now(400, 30)
 		character.draw_now(x, y)
 		x = x + 2
+		delay(0.01)
 	i = 180
 
 close_canvas()
